b3a1ebdddd82b1a38477ec3410a717dad3e99912
Cornelis <cornelisvletter@gmail.com>
2016-05-06 17:28:17 +0200

    Some changes.

--- ArtiFicialNeuralNetwork_v0.1.py
+++ ArtiFicialNeuralNetwork_v0.1.py
@@ -35,7 +35,7 @@ def calculate_loss(model):
     """
     W1, b1, W2, b2 = model['W1'], model['b1'], model['W2'], model['b2']
     
-    # Forward propagation to calculate our predictions
+    # Forward propagation
     z1 = X.dot(W1) + b1
     a1 = np.tanh(z1)
     z2 = a1.dot(W2) + b2
--- ForwardProp.py
+++ ForwardProp.py
@@ -5,6 +5,7 @@ nrows, ncols = np.shape(X)
 ncols += 1
 arc = 1
 nodes = 2
+labels = 4
 
 np.zeros(nrows)
 
@@ -13,9 +14,19 @@ X = pd.DataFrame(X)
 a1 = np.ones((nrows, ncols))
 a1[:,1:ncols] = X
 theta1 = np.random.randn(ncols, nodes)
+theta2 = np.random.randn(nodes+1, labels)
+
 
 z2 = np.dot(a1, theta1)
-a2 = np.tanh(z2)
+a2 = np.ones((nrows, nodes+1))
+a2[:,1:nodes+1] = np.tanh(z2)
+
+z3 = np.dot(a2, theta2)
+a3 = np.tanh(z3)
+
+#backward propagation
+
+
 
     
 
